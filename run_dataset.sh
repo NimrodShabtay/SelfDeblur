3c44127e826497a35b5ed09914a54a7143a08be6
NimrodShabtay <nimrod.shabtay@gmail.com>
2022-08-01 16:55:35 +0300

    add deblurring example

--- run_dataset.sh
+++ run_dataset.sh
@@ -1,3 +1,3 @@
-for i in {0..12}; do \
+for i in {0..13}; do \
     /mnt5/nimrod/nerf-pytorch/venv/bin/python3 selfdeblur_dip_defocus.py --data_path /home/nimrod/Projects/depth_from_defocus/data/mask_synt_data/downsample/small_dataset_2022_08_01 --gpu 0 --index $i; \
 done
